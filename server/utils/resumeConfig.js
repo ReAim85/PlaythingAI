f962d391259e8132c289495c5b36277f757c70bb
Vishal Jha <99897119+ReAim85@users.noreply.github.com>
2026-03-03 11:08:24 +0530

    Add copyright and license information to resumeConfig.js
    Added copyright notice and licensing information.

--- server/utils/resumeConfig.js
+++ server/utils/resumeConfig.js
@@ -1,3 +1,11 @@
+/*
+ * Copyright (C) 2026 Vishal Jha
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published
+ * by the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ */
 export const GEMINI_API_KEY = process.env.GEMINI_API_KEY;
 export const CEREBRAS_API_KEY = process.env.CEREBRAS_API_KEY;
 export const CEREBRAS_BASE_URL = "https://api.cerebras.ai/v1";
