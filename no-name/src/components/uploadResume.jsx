0b4312592b446f0d7880dea0d5dc7f5007dcd3c7
Vishal Jha <99897119+ReAim85@users.noreply.github.com>
2026-03-03 11:05:55 +0530

    Update uploadResume.jsx

--- no-name/src/components/uploadResume.jsx
+++ no-name/src/components/uploadResume.jsx
@@ -1,3 +1,11 @@
+/*
+ * Copyright (C) 2026 Vishal Jha
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published
+ * by the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ */
 // components/UploadResume.jsx
 import { useState, useRef } from 'react';
 import {
