5f95a2414e386ccc3c5752a0584c5e0046efdf34
Vishal Jha <99897119+ReAim85@users.noreply.github.com>
2026-03-03 11:06:23 +0530

    Add copyright notice to App.jsx

--- no-name/src/App.jsx
+++ no-name/src/App.jsx
@@ -1,3 +1,11 @@
+/*
+ * Copyright (C) 2026 Vishal Jha
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published
+ * by the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ */
 import { useState } from 'react';
 import LandingPage from './components/landingPage';
 import UploadResume from './components/uploadResume';
